c1025f62770ac31a9bf338b36ef1a40f3dab7899
zsg0428 <zsg258852@gmail.com>
2024-12-23 17:14:47 -0500

    adding falls tickets

--- app/places/quebec/page.js
+++ app/places/quebec/page.js
@@ -13,11 +13,16 @@ const Page = () => {
             Quebec</TitleSection>
 
         <div className='mx-auto text-center my-4'>
-            <span className='text-yellow-500 text-xl'>Wednesday, Dec 25</span>
-            <People name='圣母教堂' section='hotel'
-                    mtq='/tickets/mtl-church.pdf' qtm='Basilique Notre-Dame de Montréal.
-110 Rue Notre-Dame Ouest, Montréal'
+            <span className='text-yellow-500 text-xl'>Saturday, Dec 28</span>
+            <People name='瀑布，Parc de la Chute-Montmorency' section='hotel'
+                    mtq='/tickets/fall.pdf' qtm='5300 Sainte-Anne Blvd.
+Québec City (Québec) G1C 0M3'
             />
+        </div>
+        <div className='mx-auto text-center my-4'>
+            <span className='text-yellow-500 text-xl'>Saturday, Dec 28</span>
+            <div>晚餐:7pm at Maison Livernois</div>
+            <div><span className='text-yellow-500'> Address</span> : 1200 Rue Saint-Jean, Québec, QC G1R 1S8</div>
         </div>
     </PageLayout>
 
